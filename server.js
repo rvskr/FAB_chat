9815050e78bd73ebbd6ad1f0b724e7af182e66bf
voskreseniev <roman.mayber@gmail.com>
2024-06-20 02:01:31 +0300

    +

--- server.js
+++ server.js
@@ -1,30 +1,48 @@
+require('dotenv').config(); // Подключаем dotenv для загрузки переменных окружения из файла .env (локально)
+
 const express = require('express');
-const path = require('path');
-const firebase = require('firebase/app');
-require('firebase/auth');
-require('firebase/database');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-};
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
-firebase.initializeApp(firebaseConfig);
+app.post('/send-message', async (req, res) => {
+    const message = req.body.message;
 
-app.use(express.static(path.join(__dirname, 'public')));
+    if (!message) {
+        return res.status(400).send('Сообщение не может быть пустым');
+    }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                chat_id: TELEGRAM_CHAT_ID,
+                text: message
+            })
+        });
+
+        if (response.ok) {
+            res.status(200).send('Сообщение отправлено');
+        } else {
+            res.status(500).send('Ошибка отправки сообщения');
+        }
+    } catch (error) {
+        res.status(500).send('Ошибка отправки сообщения');
+    }
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Сервер запущен на порту ${port}`);
 });
